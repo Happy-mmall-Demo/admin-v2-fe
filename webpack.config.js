49767df2b9be3188357a4a9c297686c1b17c6efe
Redshift006 <394541649@qq.com>
2018-06-03 14:28:09 +0800

    init the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 /*
 * @Author: Reshift0023
 * @Date:   2018-05-30 10:33:43
-* @Last Modified by:   Reshift0023
-* @Last Modified time: 2018-05-30 16:24:20
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-06-02 21:22:20
 */
 const path 				= require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack 			= require('webpack');
 
 module.exports = {
 	entry: "./src/app.jsx",
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'app.js'
+		publicPath: '/dist/',
+		filename: 'js/app.js'
 	},
 	module: {
 		rules: [{
@@ -26,16 +29,58 @@ module.exports = {
 		},
 		{
 			test: /\.css$/,
+			use: ExtractTextPlugin.extract({
+				fallback: "style-loader",
+				use: "css-loader"
+			})
+		},
+		{
+			test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: ['css-loader', 'sass-loader']
+			})
+		},
+		{
+			test: /\.(png|jpg|gif)$/,
+        	use: [
+	          {
+	            loader: 'url-loader',
+	            options: {
+	              limit: 8192,
+	              name: 'resource/[name].[ext]'
+	            }
+	          }
+		]},
+
+		{
+			test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
 			use: [
-				'style-loader',
-				'css-loader'
-				]
-		}]
+				{
+					loader: 'url-loader',
+					options: {
+						limit: 8192,
+						 name: 'resource/[name].[ext]'
+					}
+				}
+			]
+		}
+
+		]
 	},
 	plugins: [
-	new HtmlWebpackPlugin({
-		template: './src/index.html'
-	})
-	]
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new ExtractTextPlugin("css/[name].css"),
+
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common',
+			filename: 'js/base.js'
+		})
+	],
+	devServer: {
+		
+	}
 };
 
