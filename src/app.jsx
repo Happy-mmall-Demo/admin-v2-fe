9a2f7d18b0991a1605c8b2231c9133df4f567b40
Redshift006 <394541649@qq.com>
2018-06-03 23:16:17 +0800

    Component practise

--- src/app.jsx
+++ src/app.jsx
@@ -10,15 +10,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'font-awesome/css/font-awesome.min.css';
-import './index.css';
-import './index.scss';
+// import 'font-awesome/css/font-awesome.min.css';
+// import './index.css';
+// import './index.scss';
+
+// ReactDOM.render(
+//   <div>
+  	
+//   	<h1>Hello, world!</h1>,
+//   </div>,
+//   document.getElementById('app')
+// );
+
+class Child extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	handleClick() {
+		this.props.changeColor('red');
+	}
+	render() {
+		return (
+			<div>
+				<h1>Parent Component Bgcolor: {this.props.bgColor}</h1>
+				<button onClick={(e) => {this.handleClick(e)}}>Change Father Component Color</button>
+			</div>
+		);
+	}
+}
+
+class Father extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bgColor: '#999'
+		}
+	}
+	
+	onBgColorChange(color) {
+		this.setState ({
+			bgColor: color
+		})
+	}
+
+	render(props) {
+		return (
+			<div style = {{background: this. state.bgColor }}>
+				<Child bgColor = {this.state.bgColor} changeColor = {(color) => {this.onBgColorChange(color)}}/>
+			</div>
+			);
+	}
+}
 
 ReactDOM.render(
-  <div>
-  	
-  	<h1>Hello, world!</h1>,
-  </div>,
-  document.getElementById('app')
+	<Father />,
+	document.getElementById('app')
 );
+
+
 
